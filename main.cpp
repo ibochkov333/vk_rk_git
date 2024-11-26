90842a7a25b5a11c29245135f6e7c12fffc8636f
ivan-bochkov <bo4koffivan123@gmail.com>
2024-11-26 16:35:55 +0300

    add Minus function

--- main.cpp
+++ main.cpp
@@ -8,9 +8,14 @@ int Multiply(int a, int b) {
 	return a * b;
 }
 
+int Minus(int a, int b) {
+	return a - b;
+}
+
 int main() {
 	std::cout << "Hello" << "\n";
 	std::cout << Sum(2, 3) << "\n";
 	std::cout << Multiply(2, 3) << "\n";
+	std::cout << Minus(5, 1) << "\n";
 	return 0;
 }
