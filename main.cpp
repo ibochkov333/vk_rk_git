2218f70307a66776c138087d3dc9ecf6d0731d10
ivan-bochkov <bo4koffivan123@gmail.com>
2024-11-26 16:32:17 +0300

    add Multiply function

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@ int Sum(int a, int b) {
 	return a + b;
 }
 
+int Multiply(int a, int b) {
+	return a * b;
+}
+
 int main() {
 	std::cout << "Hello" << "\n";
 	std::cout << Sum(2, 3) << "\n";
+	std::cout << Multiply(2, 3) << "\n";
 	return 0;
 }
